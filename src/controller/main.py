d985be9260c650ddadde5bb513c1bd0c82852c4b
chonrc <chonrc00@estudiantes.unileon.es>
2023-12-08 22:56:43 +0100

    Created Login.ui and LOgin.py. Changes in main. Error when importing Login in main.py

--- src/controller/main.py
+++ src/controller/main.py
@@ -1,8 +1,21 @@
-# Libraries.
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+print("Module Search Path:", sys.path)
+import os
+print("Current working directory:", os.getcwd())
+from view.Login import *
+
+
 
 
 def main():
-
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
-  main()
+    main()
---
+++ src/view/Login.py
@@ -0,0 +1,118 @@
+from PyQt5.QtWidgets import *
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(631, 560)
+        MainWindow.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        MainWindow.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        
+         # Get the screen geometry
+        screen_geo = QtWidgets.QDesktopWidget().screenGeometry()
+
+        # Calculate the center position for the main window
+        center_x = (screen_geo.width() - MainWindow.width()) // 2
+        center_y = (screen_geo.height() - MainWindow.height()) // 2
+
+        # Set the window position to the center
+        MainWindow.setGeometry(center_x, center_y, MainWindow.width(), MainWindow.height())
+
+
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.widget = QtWidgets.QWidget(self.centralwidget)
+        self.widget.setGeometry(QtCore.QRect(30, 30, 550, 500))
+        self.widget.setObjectName("widget")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setGeometry(QtCore.QRect(40, 30, 241, 430))
+        self.label.setStyleSheet("image: url(:/images/resources/Captura desde 2023-10-03 13-13-20.png);\n"
+"background-color: rgb(32,39,47);\n"
+"border-top-left-radius: 50px;")
+        self.label.setText("")
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.widget)
+        self.label_2.setGeometry(QtCore.QRect(270, 30, 240, 430))
+        self.label_2.setStyleSheet("background-color: rgb(111,92,76);\n"
+"border-bottom-right-radius: 50px;")
+        self.label_2.setText("")
+        self.label_2.setObjectName("label_2")
+        self.label_3 = QtWidgets.QLabel(self.widget)
+        self.label_3.setGeometry(QtCore.QRect(340, 80, 101, 40))
+        font = QtGui.QFont()
+        font.setFamily("Sans Serif")
+        font.setPointSize(20)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_3.setFont(font)
+        self.label_3.setStyleSheet("color: rgb(231, 231, 231);")
+        self.label_3.setObjectName("label_3")
+        self.lineEdit = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit.setGeometry(QtCore.QRect(295, 150, 190, 40))
+        font = QtGui.QFont()
+        font.setFamily("Sans Serif")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setWeight(50)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setStyleSheet("background-color: rgba(0, 0, 0,0);\n"
+"border: none;\n"
+"border-bottom:2px solid rgba(46, 82, 101, 200);\n"
+"color: rgb(255, 255, 255);\n"
+"padding bottom: 7px;")
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.widget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(295, 215, 190, 40))
+        font = QtGui.QFont()
+        font.setFamily("Sans Serif")
+        font.setPointSize(10)
+        self.lineEdit_2.setFont(font)
+        self.lineEdit_2.setStyleSheet("background-color: rgba(0, 0, 0,0);\n"
+"border: none;\n"
+"border-bottom:2px solid rgba(46, 82, 101, 200);\n"
+"color: rgb(255, 255, 255);\n"
+"padding bottom: 7px;")
+        self.lineEdit_2.setText("")
+        self.lineEdit_2.setEchoMode(QtWidgets.QLineEdit.Password)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton_Login = QtWidgets.QPushButton(self.widget)
+        self.pushButton_Login.setGeometry(QtCore.QRect(290, 295, 190, 40))
+        font = QtGui.QFont()
+        font.setFamily("Sans Serif")
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton_Login.setFont(font)
+        self.pushButton_Login.setStyleSheet("QPushButton#pushButton_Login{\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0.477, stop:0 rgba(11, 131, 120, 219), stop:1 rgba(85, 98, 112, 226));\n"
+"    color:rgba(255, 255, 255, 210);\n"
+"    border-radius:5px;\n"
+"}\n"
+"\n"
+"QPushButton#pushButton_Login:hover{\n"
+"    background-color: qlineargradient(spread:pad, x1:0, y1:0.505682, x2:1, y2:0.477, stop:0 rgba(150, 123, 111, 219), stop:1 rgba(85, 81, 84, 226));\n"
+"}\n"
+"\n"
+"QPushButton#pushButton_Login:pressed{\n"
+"    padding-left:5px;\n"
+"    padding-top:5px;\n"
+"    background-color:rgba(150, 123, 111, 255);\n"
+"}")
+        self.pushButton_Login.setObjectName("pushButton_Login")
+        self.label_4 = QtWidgets.QLabel(self.widget)
+        self.label_4.setGeometry(QtCore.QRect(280, 350, 221, 16))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.label_4.setFont(font)
+        self.label_4.setObjectName("label_4")
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_3.setText(_translate("MainWindow", "Log In"))
+        self.lineEdit.setPlaceholderText(_translate("MainWindow", "User Name"))
+        self.lineEdit_2.setPlaceholderText(_translate("MainWindow", "Password"))
+        self.pushButton_Login.setText(_translate("MainWindow", "L o g  I n"))
+        self.label_4.setText(_translate("MainWindow", "Forgot your User Name or Password?"))
