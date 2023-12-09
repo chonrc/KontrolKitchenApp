16527876d3ccd5ab72fcd08d81c2844a4ff09c30
chonrc <chonrc00@estudiantes.unileon.es>
2023-12-09 23:51:33 +0100

    Fully implemented Login window ui. Imports now working correctly

--- src/controller/main.py
+++ src/controller/main.py
@@ -1,18 +1,23 @@
-
-from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
-print("Module Search Path:", sys.path)
 import os
-print("Current working directory:", os.getcwd())
-from view.Login import *
 
+# Obtiene la ruta absoluta al directorio padre de este archivo
+current_directory = os.path.dirname(os.path.abspath(__file__))
+# Obtiene la ruta absoluta al directorio padre de la carpeta actual (es decir, src)
+parent_directory = os.path.abspath(os.path.join(current_directory, '..'))
 
+# Agrega el directorio padre (src) al sys.path
+sys.path.append(parent_directory)
+
+# from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtWidgets
+from view import Login
 
 
 def main():
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
-    ui = Ui_MainWindow()
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Login.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
