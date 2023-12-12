0a7e74b1923df423fee9dbbeea997a57da738cbc
DanielTorresLeon <91316017+DanielTorresLeon@users.noreply.github.com>
2023-12-12 11:31:23 +0100

    Hash Admin_controller.py

--- src/controller/Admin_controller.py
+++ src/controller/Admin_controller.py
@@ -1,12 +1,18 @@
 from model.dao.AdminDAO import *
+import bcrypt
 
 class AdminController:
     def authenticate(self, username ,password):
         admin_dao = AdminDao()
         admin_service = admin_dao.check_username_existence(username)
+
+        # Check that a unhashed password matches one that has previously been hashed
+        if  admin_service!= None and bcrypt.checkpw(password.encode('utf-8'), admin_service.password) and username == admin_service.username:
+            print("It Matches!") 
+            return True
+        else:
+            print("It Does not Match :(")
         
 
-        if admin_service != None and username == admin_service.username and password == admin_service.password:
-            return True
         
         return False
